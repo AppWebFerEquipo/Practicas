944ece75d3b72fbba658566105cd612ed646368d
AppWebFerEquipo <91996493+AppWebFerEquipo@users.noreply.github.com>
2021-10-30 15:02:10 -0500

    Add files via upload

---
+++ Resistencia/Domain/Interfaces/IOperacion.cs
@@ -0,0 +1,11 @@
+namespace Resistencia.Domain.Interfaces
+{
+    public interface IOperacion
+    {
+        void Banda_1(string Banda1 );
+        void Banda_2(string Banda2);
+        void Banda_3(string Banda3);
+        void Banda_gold(string Banda4);
+        string operacion();
+    }
+}
